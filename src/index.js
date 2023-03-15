13ee6730e1e47ad0cc74a865a762377e125fa6c7
Mauro Jr <mauro.jr@icloud.com>
2023-03-15 17:54:21 -0400

    aula 14/03 Stack Navigation

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 import { StatusBar } from 'expo-status-bar'; //importações de componentes e biliotecas
-import { StyleSheet,Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Login from './Login';
 import Signup from './Signup';
 
-export default function MeuApp() { 
-    return (
-        
-      <View style={estilo.container}> 
-          <Login/>
-        
-     
-      </View>      
-    );
-  }
+const Stack = createNativeStackNavigator();
+
+export default function MeuApp() {
+	return (
+		<View style={estilo.container}>
+			<NavigationContainer>
+				<Stack.Navigator>
+					{/*Pilha de navegação */}
+					<Stack.Screen name='Login' component={Login} />
+					{/* Tela a ser empilhada*/}
+					<Stack.Screen name='Signup' component={Signup} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</View>
+	);
+}
 
 const estilo = StyleSheet.create({
-	container:{ 
-		flex:1,
-		backgroundColor: '#fff'
-	}
-	
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
 });
